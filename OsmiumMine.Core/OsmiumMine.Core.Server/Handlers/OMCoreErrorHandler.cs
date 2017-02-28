c13a54509f094062aa443221fcad92a550bac741
0xFireball <0xFireball@outlook.com>
2017-02-28 13:31:18 -0800

    add custom error handler

---
+++ OsmiumMine.Core/OsmiumMine.Core.Server/Handlers/OMCoreErrorHandler.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using Nancy.ErrorHandling;
+
+namespace OsmiumMine.Core.Server.Handlers
+{
+    public class NotFoundHandler : IStatusCodeHandler
+    {
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            context.Response = statusCode;
+        }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            return statusCode == HttpStatusCode.NotFound
+                || statusCode == HttpStatusCode.NotImplemented;
+        }
+    }
+}
