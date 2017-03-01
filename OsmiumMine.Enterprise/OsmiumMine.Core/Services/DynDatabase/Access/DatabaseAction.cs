0ec018ce85d1d26502be84d19c0a6d233e1bfd80
0xFireball <0xFireball@outlook.com>
2017-03-01 08:17:45 -0800

    more actions

--- OsmiumMine.Enterprise/OsmiumMine.Core/Services/DynDatabase/Access/DatabaseAction.cs
+++ OsmiumMine.Enterprise/OsmiumMine.Core/Services/DynDatabase/Access/DatabaseAction.cs
@@ -10,8 +10,10 @@ namespace OsmiumMine.Core.Services.DynDatabase.Access
         Push = 1 << 2,
         Update = 1 << 3,
         Put = 1 << 4,
+        Stream = 1 << 5,
         Read = Retrieve,
         Write = Push | Update | Put,
-        All = Retrieve | Delete | Push | Update | Put
+        ReadWrite = Read | Write,
+        All = ReadWrite | Stream
     }
 }
